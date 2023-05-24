75d622304f973135f0bf973587ac626767e40a49
PR1981 <134493459+PR1981@users.noreply.github.com>
2023-05-24 11:01:24 +0100

    Adding Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
-streamlit.title("My Parents New Healthy Dinner")
+streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3')
+
+streamlit.text('kale, Spinach')
+
+streamlit.text('Hard Boiled Free-Range Egg')
