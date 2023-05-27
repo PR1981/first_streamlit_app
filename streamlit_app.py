498b65b3eda6d9c701b406bc28723221ceeefecd
PR1981 <134493459+PR1981@users.noreply.github.com>
2023-05-27 14:41:56 +0100

    typo fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-streamlit.hearder('Frutiyvice Fruit Advice!')
+streamlit.header('Frutiyvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
