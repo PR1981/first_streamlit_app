a0c365e4fbe1257b5d3608731000e9a6e945a32f
PR1981 <134493459+PR1981@users.noreply.github.com>
2023-05-27 14:41:10 +0100

    New header and json

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 #Choose the Fruit Name Column as the Index
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Banana'])
 
@@ -28,5 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.hearder('Frutiyvice Fruit Advice!')
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
