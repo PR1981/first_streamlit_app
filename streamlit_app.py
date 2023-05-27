317e88a2a610008d87bf0e126065e1f6decd3d16
PR1981 <134493459+PR1981@users.noreply.github.com>
2023-05-27 14:44:41 +0100

    improves layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# take the json verions of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output the dataframe to the screen as a table
+streamlit.dataframe(fruityvice_normalized)
