012621b5affaabad38fa0c8477ece52a6090efa1
PR1981 <134493459+PR1981@users.noreply.github.com>
2023-05-27 14:50:36 +0100

    separate fruit from url

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
 
 # take the json verions of the response and normalize it
